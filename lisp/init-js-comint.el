08edbd7301904e3a9399b8a7251d517fd7970082
Mike Chen <mike442144@hotmail.com>
2021-10-15 16:12:35 +0800

    fix error in comint init.

--- lisp/init-js-comint.el
+++ lisp/init-js-comint.el
@@ -9,5 +9,6 @@
 			 (local-set-key (kbd "C-c C-c") 'js-send-last-sexp)
 			 (local-set-key (kbd "C-x C-r") 'js-send-region)
 			 (local-set-key (kbd "C-c b") 'js-send-buffer)
+			 ))
 (provide 'init-js-comint)
 ;;; init-js-comint ends here
